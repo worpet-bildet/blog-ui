9aca1d7fc009c4bc5f9f9524e27556b8eff735e0
dachus <dachus@dachus>
2023-02-15 17:23:38 -0600

    styling and new default text

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,51 @@ import { Urbit } from '@urbit/http-api'
 import { renderToString } from 'react-dom/server'
 
 const defaultString =
-`# Start Writing Here
+`
+# Welcome to \`%blog\`
+![sigils](https://media.urbit.org/site/understanding-urbit/urbit-id/urbit-id-sigils%402x.png)
 
-Use markdown to write a file
+Use markdown to write a file. If you don't know how to write markdown, take a quick read through [this](https://www.markdownguide.org/), it's easy.
+To bind a post to a URL, just type in the path on the right and click \`Save File\`. For example, if your ships's domain is \`https://sampel-palnet.urbit.org\`, and you type in \`/blog/1\`, you will be able to see your blog at \`https://sampel-palnet.urbit.org/blog/1\`. Try it now and you will see this post in browser.
 
-Use style tags to customize the layout
+## Notes on \`<style>\`
+- Use \`<style>\` tags to customize the layout.
+- if you do not know CSS, either:
+  - learn how [here](https://www.w3schools.com/css/), or 
+  - ask ChatGPT to "write me a style sheet for markdown" and it'll know what to do
+- There is some very weird behavior because the editor and the document share a style sheet. To avoid messing up the editor, add the \`.wmde-markdown\` class in front of all styles to keep them from propogating to the editor, as I have done below. This is a bug that I'll fix at some point
+
+
+## Features Coming Soon
+- improved error handling
+- sharable themes
+- comments
+- "follow" a ship for new blog posts
+- \`%eyre\` binding dashboard
+
+For any feature requests, make an issue or a PR into [here](https://github.com/tadad/blog-ui/issues) for the UI repo, or [here](https://github.com/tadad/blog/issues) for the hoon code.
+
+Happy blogging!
+
+\`~dachus-tiprel\`
 
 <style>
-  h1 {
-    color : red;
-  }
-</style>`
+.wmde-markdown h1, h2, h3, h4, h5, h6 {
+    color : black;
+    text-align: center;
+}
+.wmde-markdown img {
+    margin: auto;
+    max-height: 300px;
+    display: block;
+}
+.wmde-markdown {
+    margin : 5rem;
+    font-size : 1.5vw;
+    color: #393939
+}
+</style>
+`
 
 function App() {
   const [api, setApi] = useState<Urbit>()
@@ -57,40 +91,6 @@ function App() {
         <MDEditor height={730} value={markdown} onChange={setMarkdown as any} data-color-mode="light"/>
       </div>
       <div className="col-span-3">
-        { bindings.length !== 0 &&
-          <ul className="list-none">
-            <label className="block text-gray-700 text-sm font-bold mb-2">Files</label>
-            { bindings.map((bind: string, i) => (
-              <li key={i} className={`d-flex justify-content-between ${fileName === bind ? 'bg-light' : ''}`}>
-                <a href={`${bind}`} target="_blank" rel="noreferrer">{bind}</a>
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
-                  onClick={async (e) => {
-                    e.preventDefault()
-                    if (!api) {
-                      console.error('api not connected')
-                      return
-                    }
-                    const res = await api.scry({
-                      app: 'blog',
-                      path: `/md${bind}`
-                    })
-                    setFileName(bind)
-                    setMarkdown(res)
-                  }}
-                >
-                  Edit
-                </button>
-                <button 
-                  className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
-                  onClick={() => { setToRemove(bind); setShowModal(true)}}
-                >
-                  Remove
-                </button>
-              </li>
-            ))}
-            </ul>
-          }
         <form
           className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
           onSubmit={async (e) => {
@@ -131,6 +131,40 @@ function App() {
             className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded w-full"
           >Save File</button>
         </form>
+        { bindings.length !== 0 &&
+          <ul className="list-none">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Files</label>
+            { bindings.map((bind: string, i) => (
+              <li key={i} className={`d-flex justify-content-between ${fileName === bind ? 'bg-light' : ''}`}>
+                <a href={`${bind}`} target="_blank" rel="noreferrer">{bind}</a>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+                  onClick={async (e) => {
+                    e.preventDefault()
+                    if (!api) {
+                      console.error('api not connected')
+                      return
+                    }
+                    const res = await api.scry({
+                      app: 'blog',
+                      path: `/md${bind}`
+                    })
+                    setFileName(bind)
+                    setMarkdown(res)
+                  }}
+                >
+                  Edit
+                </button>
+                <button 
+                  className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+                  onClick={() => { setToRemove(bind); setShowModal(true)}}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+            </ul>
+          }
       </div>
       {
         showModal && (
