2ce5b04655e867c8c03e5c4344c907e02d10f7d4
dachus <dachus@dachus>
2023-02-15 20:08:20 -0600

    default text changed

--- src/lib.tsx
+++ src/lib.tsx
@@ -3,22 +3,24 @@ export const defaultString =
 ![sigils](https://media.urbit.org/site/understanding-urbit/urbit-id/urbit-id-sigils%402x.png)
 
 Use markdown to write a file. If you don't know how to write markdown, take a quick read through [this](https://www.markdownguide.org/), it's easy.
-To bind a post to a URL, just type in the path on the right and click \`Save File\`. For example, if your ships's domain is \`https://sampel-palnet.urbit.org\`, and you type in \`/blog/1\`, you will be able to see your blog at \`https://sampel-palnet.urbit.org/blog/1\`. Try it now and you will see this post served from your ship!
+To bind a post to a URL, just type in the path on the right and click \`%save-file\`. You post will instantly be viewable from the browser. For example, if your ships's domain is \`https://sampel-palnet.urbit.org\`, and you type in \`/blog/1\`, you will be able to see your blog at \`https://sampel-palnet.urbit.org/blog/1\`. Try it now and you will see this post served from your ship!
 
 ## Notes on \`<style>\`
 - Use \`<style>\` tags to customize the layout.
 - if you do not know CSS, either:
   - learn how [here](https://www.w3schools.com/css/), or 
   - ask ChatGPT to "write me a style sheet for markdown" and it'll know what to do
-- There is some very weird behavior because the editor and the document share a style sheet. To avoid messing up the editor, add the \`.wmde-markdown\` class in front of all styles to keep them from propogating to the editor, as I have done below. This is a bug that I'll fix at some point
+- Until further notice, styling is a little wonky: 
+- The editor and the document share a style sheet
+  - To avoid messing up the editor's CSS, add the \`.wmde-markdown\` class in front of all styles to keep them from propogating, as I have done below
+  - This is a bug that I'll fix at some point
 
 
 ## Features Coming Soon
-- improved error handling
-- sharable themes
-- comments
 - "follow" a ship for new blog posts
-- \`%eyre\` binding dashboard
+- comments
+- sharable themes
+- drafts
 
 For any feature requests, make an issue or a PR into [here](https://github.com/tadad/blog-ui/issues) for the UI repo, or [here](https://github.com/tadad/blog/issues) for the hoon code.
 
