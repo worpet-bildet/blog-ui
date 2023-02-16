79af2a00b6bf36e6185863b36da7ad92978f5611
dachus <dachus@dachus>
2023-02-15 20:14:34 -0600

    changes to default text

--- src/lib.tsx
+++ src/lib.tsx
@@ -1,8 +1,9 @@
 export const defaultString =
 `# Welcome to \`%blog\`
+
 ![sigils](https://media.urbit.org/site/understanding-urbit/urbit-id/urbit-id-sigils%402x.png)
 
-Use markdown to write a file. If you don't know how to write markdown, take a quick read through [this](https://www.markdownguide.org/), it's easy.
+Use markdown to write a file. If you don't know how to write markdown, go through [this](https://www.markdowntutorial.com/), it's easy.
 To bind a post to a URL, just type in the path on the right and click \`%save-file\`. You post will instantly be viewable from the browser. For example, if your ships's domain is \`https://sampel-palnet.urbit.org\`, and you type in \`/blog/1\`, you will be able to see your blog at \`https://sampel-palnet.urbit.org/blog/1\`. Try it now and you will see this post served from your ship!
 
 ## Notes on \`<style>\`
@@ -11,8 +12,8 @@ To bind a post to a URL, just type in the path on the right and click \`%save-fi
   - learn how [here](https://www.w3schools.com/css/), or 
   - ask ChatGPT to "write me a style sheet for markdown" and it'll know what to do
 - Until further notice, styling is a little wonky: 
-- The editor and the document share a style sheet
-  - To avoid messing up the editor's CSS, add the \`.wmde-markdown\` class in front of all styles to keep them from propogating, as I have done below
+- The editor and the document share a style sheet. This screws up the editors styling, so if you e.g. add \`body : { margin : 10rem }\` to the style sheet below, everything will get screwy
+  - To avoid this, add \`.wmde-markdown\` in front of all styles to keep them from propogating, as I have done below
   - This is a bug that I'll fix at some point
 
 
