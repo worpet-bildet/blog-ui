29765f31da5838836d5bade3b4815a0dd5e2f9f4
dachus <dachus@dachus>
2023-02-28 15:20:51 -0600

    vite added

--- src/api.ts
+++
@@ -1,8 +0,0 @@
-import Urbit from '@urbit/http-api';
-// TODO fix this later
-const api = new Urbit('http://localhost', 'lidlut-tabwed-pillex-ridrup');
-api.ship = 'zod';
-// @ts-ignore TODO window typings
-window.api = api
-
-export default api
