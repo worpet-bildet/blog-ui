c2dab7cdb5c7e2ac2ef45aa6695b7838ef266b56
dachus <dachus@dachus>
2023-02-28 15:22:34 -0600

    whoops

---
+++ src/lib/defaultText.ts
@@ -0,0 +1,26 @@
+export const defaultText =
+`# Welcome to \`%blog\`
+
+![sigils](https://media.urbit.org/site/understanding-urbit/urbit-id/urbit-id-sigils%402x.png)
+
+Use markdown to write a file. If you don't know how to write markdown, go through [this](https://www.markdowntutorial.com/), it's easy.
+To bind a post to a URL, just type in the path on the right and click \`%save-file\`. You post will instantly be viewable from the browser. For example, if your ships's domain is \`https://sampel-palnet.urbit.org\`, and you type in \`/blog/1\`, you will be able to see your blog at \`https://sampel-palnet.urbit.org/blog/1\`. Try it now and you will see this post served from your ship!
+
+Use the \`%theme\`s manager to write themes using CSS
+
+## Features Coming Soon
+- \`%uqbar\` integration for paid posts
+- \`%gallery\` views managed through \`%silo\`
+
+## Fixes:
+- styling no longer impacts the editor window
+- preview is much more accurate
+- editor+preview extend the whole height of the page
+- no longer need to use .wmde-markdown for your CSS styles
+
+For any feature requests, make an issue or a PR into [here](https://github.com/tadad/blog-ui/issues) for the UI repo, or [here](https://github.com/tadad/blog/issues) for the hoon code.
+
+Happy blogging!
+
+\`~dachus-tiprel\`
+`
---
+++ src/pages/index.tsx
@@ -0,0 +1,203 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import { marked } from 'marked'
+import MDEditor from '@uiw/react-md-editor'
+import { defaultText } from '../lib/defaultText'
+import Published from '../components/BlogList'
+import Drafts from '../components/DraftsList'
+import Modal from '../components/Modal'
+import { api } from '../state/api'
+
+function Index() {
+  // inputs
+  const [fileName, setFileName] = useState('')
+  const [markdown, setMarkdown] = useState(defaultText)
+  // scries
+  const [published, setPublished] = useState<string[]>([])
+  const [drafts, setDrafts]       = useState<string[]>([])
+  const [bindings, setBindings]   = useState<any>()
+  // frontend state
+  const [rescry, setRescry]               = useState<any>()
+  const [showPreview, setShowPreview]     = useState(false)
+  const [disableSave, setDisableSave]     = useState(true)
+  const [fileNameError, setFileNameError] = useState('')
+  const [justPublished, setJustPublished] = useState('')
+
+  // scries
+  useEffect(() => {
+    const getBindings = async () => {
+      let res = await api.scry({app: 'blog', path: '/all-bindings'})
+      setBindings(res)
+    }
+    const getDrafts = async () => {
+      let res = await api.scry({app: 'blog', path: '/drafts'})
+      setDrafts(res)
+    }
+    const getPublished = async () => {
+      let res = await api.scry({app: 'blog', path: '/pages'})
+      setPublished(res)
+    }
+    getBindings()
+    getDrafts()
+    getPublished()
+  }, [api, rescry])
+
+  // frontend state
+  useEffect(() => {
+    if (fileName.charAt(fileName.length - 1) === '/') {
+      setFileNameError(`cannot end in a slash`)
+      setDisableSave(true)
+    } else if (fileName.charAt(0) !== '/'){
+      setFileNameError(`must start with a slash`)
+      setDisableSave(true)
+    } else if (bindings?.[fileName]) {
+      const inUse = bindings[fileName]
+      if (inUse === 'desk: %blog') {
+        setFileNameError(`you will overwrite ${fileName}`)
+        setDisableSave(false)
+      } else {
+        setFileNameError(`${fileName} is in use by ${inUse}`)
+        setDisableSave(true)
+      }
+    } else {
+      setFileNameError('')
+      setDisableSave(false)
+    }
+  }, [fileName, bindings])
+
+  const handlePublish = useCallback(
+    async (e : React.SyntheticEvent) => {
+      e.preventDefault()
+      const a = await api.poke({
+        app: 'blog',
+        mark: 'blog-action',
+        json: {
+          "publish": {
+            "path": fileName,
+            "html": marked.parse(markdown),
+            "md": markdown
+      }}})
+      setRescry(a)
+      setDisableSave(true)
+      setJustPublished(`${window.location.origin}${fileName}`)
+  }, [api, fileName, markdown])
+
+  const handleSaveDraft = useCallback(
+    async (e : React.SyntheticEvent) => {
+      e.preventDefault()
+      const a = await api.poke({
+        app: 'blog',
+        mark: 'blog-action',
+        json: {
+          "save-draft": {
+            "path": fileName,
+            "md": markdown
+      }}})
+      setRescry(a)
+      setDisableSave(true)
+  }, [api, fileName, markdown])
+
+  const handleDeleteDraft = useCallback(
+    async (toDelete: string) => {
+      const a = await api.poke({
+        app: 'blog',
+        mark: 'blog-action',
+        json: {
+          "delete-draft": {
+            "path": toDelete,
+      }}})
+      setRescry(a)
+      setDisableSave(true)
+  }, [api])
+
+  const handleUnpublish = useCallback(
+    async (toUnpublish: string) => {
+      const a = await api.poke({
+        app: 'blog',
+        mark: 'blog-action',
+        json: { 'unpublish': { 'path': toUnpublish } }
+      })
+      setRescry(a)
+  }, [api])
+
+  const handleEdit = useCallback(
+    async (path: string, toEdit: string) => {
+      const res = await api.scry({
+        app: 'blog',
+        path: `${path}${toEdit}` // path is either /draft or /md
+      })
+      setFileName(toEdit)
+      setMarkdown(res)
+    }, [api])
+
+  return (
+    <div className="grid grid-rows-1 lg:grid-cols-12 md:grid-cols-1 gap-4">
+      <div className="col-span-9 shadow-md flex">
+        <MDEditor
+            value={markdown}
+            onChange={(e) => {setDisableSave(false); setMarkdown(e!)}}
+            data-color-mode="light"
+            preview="edit"
+            hideToolbar
+            className={`${showPreview? 'flex-1' : 'flex-2'}`}
+        />
+        { showPreview && 
+          <iframe
+            title="preview"
+            srcDoc={`${marked.parse(markdown)}`}
+            className="flex-1"
+          />
+        }
+      </div>
+      <div className="col-span-3">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2">Name as <code>$path</code>:</label>
+            <code>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="/example/path"
+                value={fileName}
+                onChange={e => setFileName(e.target.value)}
+                pattern="^\/.+(?!\/)"
+                required
+              />
+            </code>
+            {
+              fileNameError &&
+              <p className="text-red-500 text-xs italic mt-1">{fileNameError}</p>
+            }
+          </div>
+          <div className="flex text-xs gap-x-2">
+            <button
+              className="flex-1 bg-blue-500 hover:bg-blue-700 text-white p-2 rounded w-full disabled:opacity-50"
+              disabled={disableSave || !fileName}
+              onClick={handleSaveDraft}
+            >
+              <code>%save-draft</code>
+            </button>
+            <button
+              className="flex-1 bg-blue-500 hover:bg-blue-700 text-white p-2 rounded w-full disabled:opacity-50"
+              disabled={disableSave || !fileName}
+              onClick={handlePublish}
+            >
+              <code>%publish</code>
+            </button>
+          </div>
+        </div>
+        <Published published={published} edit={handleEdit} remove={handleUnpublish}/>
+        <Drafts drafts={drafts} edit={handleEdit} remove={handleDeleteDraft}/>
+        <label>
+          <input
+            type="checkbox"
+            checked={showPreview}
+            onChange={() => setShowPreview(!showPreview)}
+          />
+          <code className="ml-2">%show-preview</code>
+        </label>
+      </div>
+      { justPublished.length !== 0 && <Modal justPublished={justPublished} setJustPublished={setJustPublished}/> }
+    </div>
+  );
+}
+
+export default Index
