b12fcace4bedec16be7c04bad70cd22100c9b07a
dachus <dachus@dachus>
2023-03-02 14:53:15 -0600

    sidebar scroll working

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -45,90 +45,88 @@ export default function SideBar() {
   }, [])
 
   return (
-    <>
-      <div className="overflow-y-scroll">
-        <Link to="/">
-          <h1 className="text-3xl"><code>%blog</code></h1>
-        </Link>
-        <ul className="bg-white px-4 pt-6">
-          <label className="block text-gray-700 font-bold mb-3">Published <code>%blog</code>s</label>
-          { pages.sort().map((pub: string, i) => (
-            <li key={i} className="flex mb-1 text-blue-600 visited:text-purple-600 text-xs">
-              <div className="text-left flex-1 my-auto truncate">
-                <a href={`${pub}`} target="_blank" rel="noreferrer"><code>{pub}</code></a>
-              </div>
-              <div className="flex-1 flex justify-end">
-                <Link to={`/published${pub}`}>
-                  <button className="bg-yellow-500 hover:bg-yellow-700 text-white p-2 rounded disabled:opacity-50">
-                    <PencilSquareIcon className="w-4 h-4" style={{ color : 'white' }}/>
-                  </button>
-                </Link>
-                <button
-                  className="bg-red-500 hover:bg-red-700 text-white p-2 rounded disabled:opacity-50 ml-1"
-                  onClick={() => handleUnpublish  (pub)}
-                >
-                  <TrashIcon className="w-4 h-4" style={{ color : 'white' }}/>
+    <div className="h-full overflow-y-scroll">
+      <Link to="/">
+        <h1 className="text-3xl"><code>%blog</code></h1>
+      </Link>
+      <ul className="bg-white px-4 pt-6">
+        <label className="block text-gray-700 font-bold mb-3">Published <code>%blog</code>s</label>
+        { pages.sort().map((pub: string, i) => (
+          <li key={i} className="flex mb-1 text-blue-600 visited:text-purple-600 text-xs">
+            <div className="text-left flex-1 my-auto truncate">
+              <a href={`${pub}`} target="_blank" rel="noreferrer"><code>{pub}</code></a>
+            </div>
+            <div className="flex-1 flex justify-end">
+              <Link to={`/published${pub}`}>
+                <button className="bg-yellow-500 hover:bg-yellow-700 text-white p-2 rounded disabled:opacity-50">
+                  <PencilSquareIcon className="w-4 h-4" style={{ color : 'white' }}/>
                 </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <ul className="bg-white px-4 pt-6">
-          <label className="block text-gray-700 font-bold mb-3"><code>%draft</code>s</label>
-          { drafts.sort().map((draft: string, i) => (
-            <li key={i} className="flex mb-1 text-xs">
-              <div className="text-left flex-1 my-auto truncate">
-                <code>{draft}</code>
-              </div>
-              <div className="flex-1 flex justify-end">
-                <Link to={`/draft${draft}`}>
-                  <button className="bg-yellow-500 hover:bg-yellow-700 text-white p-2 rounded disabled:opacity-50">
-                    <PencilSquareIcon className="w-4 h-4" style={{ color : 'white' }}/>
-                  </button>
-                </Link>
-                <button
-                  className="bg-red-500 hover:bg-red-700 text-white p-2 rounded disabled:opacity-50 ml-1"
-                  onClick={() => handleDeleteDraft(draft)}
-                >
-                  <TrashIcon className="w-4 h-4" style={{ color : 'white' }}/>
+              </Link>
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white p-2 rounded disabled:opacity-50 ml-1"
+                onClick={() => handleUnpublish  (pub)}
+              >
+                <TrashIcon className="w-4 h-4" style={{ color : 'white' }}/>
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <ul className="bg-white px-4 pt-6">
+        <label className="block text-gray-700 font-bold mb-3"><code>%draft</code>s</label>
+        { drafts.sort().map((draft: string, i) => (
+          <li key={i} className="flex mb-1 text-xs">
+            <div className="text-left flex-1 my-auto truncate">
+              <code>{draft}</code>
+            </div>
+            <div className="flex-1 flex justify-end">
+              <Link to={`/draft${draft}`}>
+                <button className="bg-yellow-500 hover:bg-yellow-700 text-white p-2 rounded disabled:opacity-50">
+                  <PencilSquareIcon className="w-4 h-4" style={{ color : 'white' }}/>
                 </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <ul className="bg-white px-4 pt-6">
-          <label className="block text-gray-700 font-bold mb-3"><code>%theme</code>s</label>
-          { themes.sort().map((theme: string, i) => (
-            <li key={i} className="flex mb-1 text-xs">
-              <div className="text-left flex-1 my-auto truncate">
-                <code>%{theme}</code>
-              </div>
-              <div className="flex-1 flex justify-end">
-                <Link to={`/theme/${theme}`}>
-                  <button
-                    className="bg-yellow-500 hover:bg-yellow-700 text-white p-2 rounded disabled:opacity-50"
-                  >
-                    <PencilSquareIcon className="w-4 h-4" style={{ color : 'white' }}/>
-                  </button>
-                </Link>
+              </Link>
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white p-2 rounded disabled:opacity-50 ml-1"
+                onClick={() => handleDeleteDraft(draft)}
+              >
+                <TrashIcon className="w-4 h-4" style={{ color : 'white' }}/>
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <ul className="bg-white px-4 pt-6">
+        <label className="block text-gray-700 font-bold mb-3"><code>%theme</code>s</label>
+        { themes.sort().map((theme: string, i) => (
+          <li key={i} className="flex mb-1 text-xs">
+            <div className="text-left flex-1 my-auto truncate">
+              <code>%{theme}</code>
+            </div>
+            <div className="flex-1 flex justify-end">
+              <Link to={`/theme/${theme}`}>
                 <button
-                  className="bg-red-500 hover:bg-red-700 text-white p-2 rounded disabled:opacity-50 ml-1"
-                  onClick={() => handleDeleteTheme(theme)}
+                  className="bg-yellow-500 hover:bg-yellow-700 text-white p-2 rounded disabled:opacity-50"
                 >
-                  <TrashIcon className="w-4 h-4" style={{ color : 'white' }}/>
+                  <PencilSquareIcon className="w-4 h-4" style={{ color : 'white' }}/>
                 </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <Link to="/theme">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded disabled:opacity-50"
-          >
-            <code>%new-theme</code>
-          </button>
-        </Link>
-      </div>
-    </>
+              </Link>
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white p-2 rounded disabled:opacity-50 ml-1"
+                onClick={() => handleDeleteTheme(theme)}
+              >
+                <TrashIcon className="w-4 h-4" style={{ color : 'white' }}/>
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <Link to="/theme">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded disabled:opacity-50"
+        >
+          <code>%new-theme</code>
+        </button>
+      </Link>
+    </div>
   )
 }
