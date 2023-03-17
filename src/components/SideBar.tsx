5481c318012d7d7a2a2b79bb252dba648a8d5ff5
morelazers <contacttomnash@gmail.com>
2023-03-17 14:59:47 +0100

    feat: accordion sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,11 @@
-import { useState, useCallback } from 'react'
-import { Link, useMatch } from 'react-router-dom'
-import { TrashIcon, ArchiveBoxXMarkIcon } from '@heroicons/react/24/outline'
+import { useState, useCallback, useEffect } from 'react'
+import { Link, useMatch, useNavigate } from 'react-router-dom'
+import {
+  TrashIcon,
+  ArchiveBoxXMarkIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from '@heroicons/react/24/outline'
 import { useStore } from '../state/base'
 import { ConfirmDeleteDraft, ConfirmUnpublish } from './Modal'
 import { api } from '../state/api'
@@ -9,14 +14,53 @@ interface SideBarProps {
   onToggle: any
 }
 
+type SidebarEntry = {
+  path: string
+  children: string[]
+}
+
 export default function SideBar({ onToggle }: SideBarProps) {
   const drafts = useStore((state) => state.drafts)
   const pages = useStore((state) => state.pages)
   const themes = useStore((state) => state.themes)
   const { getAll } = useStore()
+  const navigate = useNavigate()
   const [fileName, setFileName] = useState('')
   const [showDeleteDraftModal, setShowDeleteDraftModal] = useState(false)
   const [showUnpublishModal, setShowUnpublishModal] = useState(false)
+
+  const [nestedDrafts, setNestedDrafts] = useState<SidebarEntry[]>([])
+  const [nestedPages, setNestedPages] = useState<SidebarEntry[]>([])
+
+  useEffect(() => {
+    setNestedDrafts(nestPaths(drafts, '/drafts'))
+    setNestedPages(nestPaths(pages, '/published'))
+  }, [pages, drafts])
+
+  const nestPaths = (paths: string[], linkbase: string) => {
+    return paths
+      .map((d) => ({
+        path: parentPath(d),
+        children: paths.filter((_d) => parentPath(_d) === parentPath(d)),
+        linkbase,
+      }))
+      .filter((d) => !!d)
+      .reduce((acc: SidebarEntry[], cur) => {
+        if (acc.length === 0) return [...acc, cur]
+        if (!acc.find((d) => d.path === cur.path)) return [...acc, cur]
+        return acc
+      }, [])
+      .map((path) =>
+        path && path.children && path.children.length === 1
+          ? { ...path, path: path.children[0] }
+          : path
+      )
+  }
+
+  const parentPath = (path: string) => {
+    return path.split('/').slice(0, 2).join('/')
+  }
+
   const match = useMatch('*')
 
   const handleDeleteDraft = useCallback(async (toDelete: string) => {
@@ -56,38 +100,73 @@ export default function SideBar({ onToggle }: SideBarProps) {
 
   type SidebarItemProps = {
     linkbase: string
-    item: string
+    item: SidebarEntry
   }
 
   const SidebarItem = ({ linkbase, item }: SidebarItemProps) => {
-    let linkto = `${linkbase}${item}`
+    let linkto = `${linkbase}${item.path}`
+    const [open, setOpen] = useState(false)
+    const hasChildren = item.children.length > 1
     return (
-      <li
-        className={`flex flex-row justify-between items-center mb-1 text-blue-600 visited:text-purple-600 text-xs hover:bg-gray-100 p-2 ${
-          match?.pathname === linkto ? 'bg-gray-100' : ''
-        }`}
-      >
-        <Link to={linkto}>
-          <div className='text-left flex-1 my-auto truncate'>
-            <code>{item}</code>
-          </div>
-        </Link>
-        {linkbase !== '/theme/' && (
-          <div
-            className='w-6 p-1 cursor-pointer rounded-sm text-red-500 hover:text-white hover:bg-red-500 '
-            onClick={() => {
-              showModal(linkbase)
-              setFileName(item)
-            }}
-          >
-            {linkbase === '/published' ? (
-              <ArchiveBoxXMarkIcon />
-            ) : (
-              <TrashIcon />
-            )}
-          </div>
-        )}
-      </li>
+      <>
+        <li
+          className={`flex cursor-pointer pointer-events-auto justify-between mb-1 text-blue-600 visited:text-purple-600 text-xs hover:bg-gray-100 p-2 ${
+            match?.pathname === linkto ? 'bg-gray-100' : ''
+          } ${
+            hasChildren ? 'flex-col justify-center' : 'flex-row  items-center'
+          }`}
+          onClick={() => {
+            if (hasChildren) return
+            navigate(linkto)
+          }}
+        >
+          {!hasChildren && (
+            <>
+              <div className='text-left flex-1 my-auto truncate'>
+                <code>{item.path}</code>
+              </div>
+              {linkbase !== '/theme/' && (
+                <div
+                  className='w-6 p-1 cursor-pointer rounded-sm text-red-500 hover:text-white hover:bg-red-500 '
+                  onClick={() => {
+                    showModal(linkbase)
+                    setFileName(item.path)
+                  }}
+                >
+                  {linkbase === '/published' ? (
+                    <ArchiveBoxXMarkIcon />
+                  ) : (
+                    <TrashIcon />
+                  )}
+                </div>
+              )}
+            </>
+          )}
+          {hasChildren && (
+            <div
+              className='flex flex-row w-full justify-between pointer-events-auto'
+              onClick={() => {
+                setOpen(!open)
+              }}
+            >
+              <div className='text-left flex-1 my-auto truncate'>
+                <code>{item.path}</code>
+              </div>
+              <div className='w-6'>
+                {open ? <ChevronUpIcon /> : <ChevronDownIcon />}
+              </div>
+            </div>
+          )}
+        </li>
+        {open &&
+          item.children.map((p) => (
+            <SidebarItem
+              item={{ path: p, children: [] }}
+              linkbase={linkbase}
+              key={p}
+            />
+          ))}
+      </>
     )
   }
 
@@ -105,7 +184,7 @@ export default function SideBar({ onToggle }: SideBarProps) {
         <label className='block text-gray-700 font-bold mb-3'>
           Published <code>%blog</code>s
         </label>
-        {pages.sort().map((pub: string, i) => (
+        {nestedPages.sort().map((pub: SidebarEntry, i) => (
           <SidebarItem linkbase={`/published`} item={pub} key={i}></SidebarItem>
         ))}
       </ul>
@@ -113,7 +192,7 @@ export default function SideBar({ onToggle }: SideBarProps) {
         <label className='block text-gray-700 font-bold mb-3'>
           <code>%draft</code>s
         </label>
-        {drafts.sort().map((draft: string, i) => (
+        {nestedDrafts.sort().map((draft: SidebarEntry, i) => (
           <SidebarItem linkbase={`/draft`} item={draft} key={i}></SidebarItem>
         ))}
       </ul>
@@ -122,7 +201,11 @@ export default function SideBar({ onToggle }: SideBarProps) {
           <code>%theme</code>s
         </label>
         {themes.sort().map((theme: string, i) => (
-          <SidebarItem linkbase={`/theme/`} item={theme} key={i}></SidebarItem>
+          <SidebarItem
+            linkbase={`/theme/`}
+            item={{ path: theme, children: [] }}
+            key={i}
+          ></SidebarItem>
         ))}
       </ul>
       <Link to='/theme'>
